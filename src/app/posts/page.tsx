e32b5a9b1371692da5cca652d6a6f60716b12f12
김명욱 <anko@gsitm.com>
2024-03-13 11:44:34 +0900

    cate post

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,3 +1,12 @@
-export default function PostsPage() {
-  return <div>PostsPage</div>;
+import FilterablePosts from "@/components/FilterablePosts";
+import { getAllPosts } from "@/api/posts";
+
+export default async function PostsPage() {
+  const posts = await getAllPosts();
+  const categories = [...new Set(posts.map((post) => post.category))];
+  return (
+    <>
+      <FilterablePosts posts={posts} categories={categories} />
+    </>
+  );
 }
