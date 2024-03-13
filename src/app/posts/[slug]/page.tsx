3d56e285c38825b951d3f89d69db16ea1258b6f5
김명욱 <anko@gsitm.com>
2024-03-13 13:05:19 +0900

    마크다운뷰어 추가

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,3 +1,36 @@
-export default function PostPage() {
-  return <div></div>;
+import { getPostData } from "@/api/posts";
+import MarkdownViewer from "@/components/MarkdownViewer";
+import Image from "next/image";
+import { CiCalendar } from "react-icons/ci";
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function PostPage({ params: { slug } }: Props) {
+  const { title, description, content, date, path } = await getPostData(slug);
+
+  return (
+    <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
+      <Image
+        className="w-full h-1/5 max-h-[500px] object-cover"
+        src={`/images/posts/${path}.png`}
+        alt={title}
+        width={760}
+        height={420}
+      />
+      <section className="flex flex-col p-4">
+        <div className="flex items-center self-end text-sky-600">
+          <CiCalendar />
+          <p className="font-semibold ml-2">{date.toString()}</p>
+        </div>
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <p className="text-xl font-bold">{description}</p>
+        <div className="w-44 border-2 border-sky-600 mt-4 mb-8"></div>
+        <MarkdownViewer content={content} />
+      </section>
+    </article>
+  );
 }
